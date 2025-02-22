f954ac98c46a50fe9585eb1aa372b8c412f90b74
Leonardo Ribeiro de Azevedo <leonardo.razevedo@americanas.io>
2025-02-22 17:06:14 -0300

    Adicionando variáveis como aprendizado somente. Podendo excluí-las, caso necessário.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L; // quando for long, tem que terminar com "L"
+        float pi = 3.14F; // quando for float, tem que terminar com "F"
+        double salario = 1275.33;
+        double salarioMinimo = 2500;
     }
 }
