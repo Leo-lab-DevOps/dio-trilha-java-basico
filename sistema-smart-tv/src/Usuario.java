e7a0f8d03776cf31a7f899643788117567a71fca
Leonardo Ribeiro de Azevedo <leonardo.razevedo@americanas.io>
2025-02-27 18:42:38 -0300

    Adicionando mais uma funcionalidade de mudar canal, aumentar volume,...etc..

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -2,6 +2,15 @@ public class Usuario {
     public static void main(String[] args) throws Exception {
         SmartTv smartTv = new SmartTv();
 
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual : " + smartTv.volume);
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual : " + smartTv.canal);
+
         System.out.println("TV Ligada ? " + smartTv.ligada);
         System.out.println("Canal Atual : " + smartTv.canal);
         System.out.println("Volume Atual : " + smartTv.volume);
